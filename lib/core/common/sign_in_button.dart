80bb1da0e1c09576bbc57086b5efb2627179cdb8
crossword <abhishekverma7t@gmail.com>
2024-02-08 18:53:10 +0530

    minor change on elevated google sign in button

--- lib/core/common/sign_in_button.dart
+++ lib/core/common/sign_in_button.dart
@@ -1,15 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:reddit_clone/theme/palette.dart';
+
+import '../constants/constants.dart';
 
 class SignInButton extends StatelessWidget {
   const SignInButton({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return ElevatedButton.icon(onPressed: (){
-      debugPrint('Elevated button clicke');
-    },
-        icon: const Icon(Icons.ac_unit_sharp),
-        label: const Text('Sign in with Google.')
+    return Padding(
+      padding: const EdgeInsets.all(18.0),
+      child: ElevatedButton.icon(onPressed: (){
+        debugPrint('Elevated button clicked');
+      },
+          icon: Image.asset(Constants.googleLogoPath, width: 35,),
+          label: const Text('Continue with Google',
+          style: TextStyle(
+            fontSize: 18,
+            color: Colors.white,
+          ),),
+          style: ElevatedButton.styleFrom(
+            backgroundColor: Palette.greyColor,
+            minimumSize: const Size(double.infinity, 50),
+          )
+      ),
     );
   }
 }
