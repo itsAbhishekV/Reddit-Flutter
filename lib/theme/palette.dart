c68438578284aef6da18d394f290ebfd3b698dbf
itsAbhishekV <abhishekverma7t@gmail.com>
2026-07-22 10:25:00 +0530

    use riverpod legacy import in palette

--- lib/theme/palette.dart
+++ lib/theme/palette.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_riverpod/legacy.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 final themeNotifierProvider =
